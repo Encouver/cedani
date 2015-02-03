ac6393642b33539f0937c8a12a14e0141875816c
Marcos Ramírez <marcospha@gmail.com>
2015-02-02 22:56:19 -0430

    Navegación CRUDs
    Menú de navegación para los CRUDs

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,7 +1,10 @@
 <?php
 /* @var $this yii\web\View */
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Menu;
 
-$this->title = 'My Yii Application';
+$this->title = 'Administración CEDANI';
 ?>
 <div class="site-index">
 
@@ -11,7 +14,30 @@ $this->title = 'My Yii Application';
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+
     </div>
+    <?php 
+        NavBar::begin(['brandLabel' => '']);
+        echo Nav::widget([
+            'items' => [
+                ['label' => 'Clientes', 'url' => ['/clientes/index']],
+                ['label' => 'Productos', 'url' => ['/productos/index']],
+                ['label' => 'Tareas', 'url' => ['/tareas/index']],
+                ['label' => 'Empleados', 'url' => ['/empleados/index']],
+                ['label' => 'Configuraciones', 'url' => ['/coniguraciones/index']],
+                ['label' => 'Productos Proveedores', 'url' => ['/productos-proveedores/index']],
+                ['label' => 'Inventarios', 'url' => ['/inventarios/index']],
+                ['label' => 'Histórico de precios', 'url' => ['/historico-precios/index']],
+                ['label' => 'Nótas de Crédito', 'url' => ['/notas-de-credito/index']],
+                ['label' => 'Cobranzas', 'url' => ['/cobranzas/index']],
+                ['label' => 'Entregas', 'url' => ['/entregas/index']],
+                ['label' => 'Facturas', 'url' => ['/facturas/index']],
+                ['label' => 'Compras', 'url' => ['/compras/index']],
+            ],
+        ]);
+        NavBar::end();
+
+    ?>
 
     <div class="body-content">
 
